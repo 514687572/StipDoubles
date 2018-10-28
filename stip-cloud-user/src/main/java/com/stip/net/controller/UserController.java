894393574ec3738149f4d26ef6ca5250e12bc288
514687572 <514687572@qq.com>
2018-10-28 10:54:41 +0800

    增加配置中心消费端

--- stip-cloud-user/src/main/java/com/stip/net/controller/UserController.java
+++ stip-cloud-user/src/main/java/com/stip/net/controller/UserController.java
@@ -2,6 +2,7 @@ package com.stip.net.controller;
 
 import com.stip.net.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,12 @@ public class UserController {
     @Autowired
     public RedisService redisService;
 
-    @RequestMapping("/index/{name}")
-    public String index(@PathVariable("name") String name) {
-        return redisService.hello(name);
+    @Value("${name}")
+    private String name;
+
+
+    @RequestMapping("/index")
+    public String index() {
+        return redisService.hello(this.name);
     }
 }
