eaca7c274552d56c15f9225fc8b87d57baa86074
514687572 <514687572@qq.com>
2018-10-28 12:29:48 +0800

    增加配置中心消费端

--- stip-cloud-user/src/main/java/com/stip/net/StipCloudUserApplication.java
+++ stip-cloud-user/src/main/java/com/stip/net/StipCloudUserApplication.java
@@ -3,16 +3,15 @@ package com.stip.net;
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
 @EnableDiscoveryClient
 public class StipCloudUserApplication {
